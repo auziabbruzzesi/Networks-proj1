a9f10a9496fe5467a3ea2027416bbfad70df7fe6
auziabbruzzesi <austinta@udel.edu>
2018-04-01 16:35:09 -0400

    ?

--- tcpserver.c
+++ tcpserver.c
@@ -152,7 +152,7 @@ int main(void) {
                  j++;
                  int k;
                  for(k = 0; k < 80; k++){
-                     printf("%s",packet.data[k]);
+                     printf("%s",packet.data);
                  }
                 }
                 fclose ( file );
