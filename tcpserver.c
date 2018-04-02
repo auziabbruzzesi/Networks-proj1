4ee3662d712b85c7fd6d0ab642cd0d66a445b144
auziabbruzzesi <austinta@udel.edu>
2018-04-02 14:37:42 -0400

    print statement again

--- tcpserver.c
+++ tcpserver.c
@@ -143,7 +143,7 @@ int main(void) {
                     // printf("bytes sent: %d \n",bytes_sent);
                     bytes_sent = 0;
 					bytes_sent = send(sock_connection, &p.data, p.header.count, 0);//send data
-                    printf("sizeof(p.data): %lu\n",sizeof(*p.data));
+                    printf("sizeof(p.data): %d\n",sizeof(p.data));
                     printf("bytes sent: %d \n",bytes_sent);
 
 					printf("Sent line is:\n");
