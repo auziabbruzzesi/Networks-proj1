dbbb71482df4c3c37873b54f9e36d8f4d0f26aee
auziabbruzzesi <austinta@udel.edu>
2018-04-03 12:48:53 -0400

    got rid of free

--- tcpclient.c
+++ tcpclient.c
@@ -134,9 +134,10 @@ int main(void) {
   		printf("header stuff %d\n", h.count);
   		printf("size of message: %lu\n",strlen(message));
  	 	printf("%s \n",message);
-          //fprintf(file, "%s", message);
-          if(!h.count){break;}
-        free(message);
+        
+        //fprintf(file, "%s", message);
+        if(!h.count){break;}
+        //free(message);
         
         }
 		
