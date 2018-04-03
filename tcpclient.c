48a3050d7ae04c27a0d2ff1a90b547a0a00f122a
auziabbruzzesi <austinta@udel.edu>
2018-04-03 19:40:32 -0400

    hoping this doesn't break anything

--- tcpclient.c
+++ tcpclient.c
@@ -17,145 +17,132 @@ typedef struct{
 }Header;
 
 //header constructor method
-Header new_header(short seq, short count){
+Header new_header(short seq, short count) {
     Header h;
     h.packet_sequence_num = seq;
     h.count = count;
     return h;
-    
+
 }
 
-typedef struct{
+typedef struct {
     Header header;
     char * data;
 
-}Packet;
+}
+Packet;
 
-Packet new_packet(short seq,short count,char * data){
+Packet new_packet(short seq, short count, char * data) {
     Packet p;
-    p.header = new_header(seq,count);
+    p.header = new_header(seq, count);
     p.data = data;
     return p;
 }
 int main(void) {
 
-   int sock_client;  /* Socket used by client */
+    int sock_client; /* Socket used by client */
 
-   struct sockaddr_in server_addr;  /* Internet address structure that
-                                        stores server address */
-   struct hostent * server_hp;      /* Structure to store server's IP
-                                        address */
-   char server_hostname[STRING_SIZE]; /* Server's hostname */
-   unsigned short server_port;  /* Port number used by server (remote port) */
+    struct sockaddr_in server_addr;
+    /* Internet address structure that
+                                          stores server address */
+    struct hostent * server_hp;
+    /* Structure to store server's IP
+                                      address */
+    char server_hostname[STRING_SIZE]; /* Server's hostname */
+    unsigned short server_port; /* Port number used by server (remote port) */
 
-   char sentence[STRING_SIZE];  /* send message */
-   char modifiedSentence[STRING_SIZE]; /* receive message */
-   unsigned int msg_len;  /* length of message */                      
-   int bytes_sent, bytes_recd; /* number of bytes sent or received */
+    char sentence[STRING_SIZE]; /* send message */
+    char modifiedSentence[STRING_SIZE]; /* receive message */
+    unsigned int msg_len; /* length of message */
+    int bytes_sent, bytes_recd; /* number of bytes sent or received */
     //another test
-   /* open a socket */
+    /* open a socket */
 
-   if ((sock_client = socket(PF_INET, SOCK_STREAM, IPPROTO_TCP)) < 0) {
-      perror("Client: can't open stream socket");
-      exit(1);
-   }
+    if ((sock_client = socket(PF_INET, SOCK_STREAM, IPPROTO_TCP)) < 0) {
+        perror("Client: can't open stream socket");
+        exit(1);
+    }
 
-   /* Note: there is no need to initialize local client address information 
-            unless you want to specify a specific local port
-            (in which case, do it the same way as in udpclient.c).
-            The local address initialization and binding is done automatically
-            when the connect function is called later, if the socket has not
-            already been bound. */
+    /* Note: there is no need to initialize local client address information 
+    unless you want to specify a specific local port
+    (in which case, do it the same way as in udpclient.c).
+    The local address initialization and binding is done automatically
+    when the connect function is called later, if the socket has not
+    already been bound. */
 
-   /* initialize server address information */
+    /* initialize server address information */
 
-   printf("Enter hostname of server: ");
-   //scanf("%s", server_hostname);
-   strcpy(server_hostname,"localhost" );
-   if ((server_hp = gethostbyname(server_hostname)) == NULL) {
-      perror("Client: invalid server hostname");
-      close(sock_client);
-      exit(1);
-   }
+    printf("Enter hostname of server: ");
+    //scanf("%s", server_hostname);
+    strcpy(server_hostname, "localhost");
+    if ((server_hp = gethostbyname(server_hostname)) == NULL) {
+        perror("Client: invalid server hostname");
+        close(sock_client);
+        exit(1);
+    }
 
-   printf("Enter port number for server: ");
-   //scanf("%hu", &server_port);
-   server_port = 45678;
+    printf("Enter port number for server: ");
+    //scanf("%hu", &server_port);
+    server_port = 45678;
 
-   /* Clear server address structure and initialize with server address */
-   memset(&server_addr, 0, sizeof(server_addr));
-   server_addr.sin_family = AF_INET;
-   memcpy((char *)&server_addr.sin_addr, server_hp->h_addr,
-                                    server_hp->h_length);
-   server_addr.sin_port = htons(server_port);
+    /* Clear server address structure and initialize with server address */
+    memset( & server_addr, 0, sizeof(server_addr));
+    server_addr.sin_family = AF_INET;
+    memcpy((char * ) & server_addr.sin_addr, server_hp - > h_addr,
+        server_hp - > h_length);
+    server_addr.sin_port = htons(server_port);
 
     /* connect to the server */
- 		
-   if (connect(sock_client, (struct sockaddr *) &server_addr, 
-                                    sizeof (server_addr)) < 0) {
-      perror("Client: can't connect to server");
-      close(sock_client);
-      exit(1);
-   }
-  
-   /* user interface */
 
-   printf("Please input the file name:\n");
-   scanf("%s", sentence);
-   msg_len = strlen(sentence) + 1;
-   FILE * file;
-   file = fopen("out.txt","wb");
-   /* send message */
-   bytes_sent = send(sock_client, sentence, msg_len, 0);
-
-   /* get response from server */
-  //ssize_t recv(int socket, void *buffer, size_t length, int flags);
-  // I need 2 recv's -- one for the header and one for the data
-  Header h;
-  size_t message_bytes = 1;
-  int num = 81;
-  int package = 0;
-  int seq = 0;
-  int data_bytes = 0;
-  int counter;
-  while(message_bytes){
-   char * message = (char*)malloc((80)*sizeof(char));
-    bytes_recd = recv(sock_client,&h, sizeof(long), 0); 
-    seq = ntohs(h.packet_sequence_num);
-    data_bytes = ntohs(h.count);
-    counter += data_bytes;
-    if(!data_bytes){
-        printf("TCP CLIENT: End of Transmission Packet with sequence number %d received with %d data bytes\n", seq, data_bytes);
-    }else{
-    printf("TCP CLIENT:Packet %d received with %d data bytes\n",seq,data_bytes);
+    if (connect(sock_client, (struct sockaddr * ) & server_addr,
+            sizeof(server_addr)) < 0) {
+        perror("Client: can't connect to server");
+        close(sock_client);
+        exit(1);
     }
-    fflush(stdout);
-    //printf("sequence number is %d\n", seq);
-    //printf("header bytes received = %d \n", bytes_recd);
-    bytes_recd= 0;
-    bytes_recd = recv(sock_client,message,80,0);
-    message_bytes = bytes_recd;
-    //printf("data bytes received = %d \n", bytes_recd);
-    //printf("header stuff %d\n", h.count);
-    //printf("length of message: %lu\n",strlen(message));
-    //printf("size of message: %lu \n", sizeof(message));
 
-    // printf("%s \n",message);
-    fprintf(file, "%s",message);
-    package+=1;
+    /* user interface */
 
-    
-  }
+    printf("Please input the file name:\n");
+    scanf("%s", sentence);
+    msg_len = strlen(sentence) + 1;
+    FILE * file;
+    file = fopen("out.txt", "wb");
+    /* send message */
+    bytes_sent = send(sock_client, sentence, msg_len, 0);
 
+    Header h;
+    size_t message_bytes = 1;
 
-  printf("STATS:\n");
-  printf("SERVER: number of packets transmitted: %d\n",seq);
-  printf("SERVER: total data bytes sent: %d \n", counter);
-  //    printf("\nThe response from server is:\n");
-  //    printf("%s\n\n", modifiedSentence);
+    int seq = 0;
+    int data_bytes = 0;
+    int counter;
+    while (message_bytes) {
+        char * message = (char * ) malloc((80) * sizeof(char));
+        bytes_recd = recv(sock_client, & h, sizeof(long), 0);
+        seq = ntohs(h.packet_sequence_num);
+        data_bytes = ntohs(h.count);
+        counter += data_bytes;
+        if (!data_bytes) {
+            printf("TCP CLIENT: End of Transmission Packet with sequence number %d received with %d data bytes\n", seq, data_bytes);
+        } else {
+            printf("TCP CLIENT:Packet %d received with %d data bytes\n", seq, data_bytes);
+        }
+        fflush(stdout);
+        bytes_recd = 0;
+        bytes_recd = recv(sock_client, message, 80, 0);
+        message_bytes = bytes_recd;
+        fprintf(file, "%s", message);
 
-   /* close the socket */
+    }
 
+    printf("STATS:\n");
+    printf("SERVER: number of packets transmitted: %d\n", seq);
+    printf("SERVER: total data bytes sent: %d \n", counter);
+    //    printf("\nThe response from server is:\n");
+    //    printf("%s\n\n", modifiedSentence);
 
-   close (sock_client);
+    /* close the socket */
+
+    close(sock_client);
 }
